0553d50d11f8a4537a6023d078f2122dfc42c9c0
Ayodeji Olabisi <yinka.olabisi15@gmail.com>
2021-12-05 10:05:39 +0100

    Tweet Integration continued
    Tweepy

--- main.py
+++ main.py
@@ -1,28 +1,107 @@
-from flask import Flask
-from flask import request
+# import modules
+from flask import Flask, render_template, request, redirect, url_for
 import flask
-import tweepy
+from urllib.parse import urlparse, urlsplit
+import re
+from instaloader import *
 from dotenv import load_dotenv
 import os
+import base64
+import requests
+import tweepy
 
+# initialise flask app
 app = Flask(__name__)
 
-# config
-
+# initialise environment variables - instagram credentials
 load_dotenv()
-client = os.getenv("CONSUMER_TOKEN")
-secret = os.getenv("CONSUMER_SECRET")
-
-CONSUMER_TOKEN = client
-CONSUMER_SECRET = secret
+client = os.getenv("USERNAME")
+secret = os.getenv("PASSWORD")
+twitter_client = os.getenv("CONSUMER_TOKEN")
+twitter_secret = os.getenv("CONSUMER_SECRET")
 CALLBACK_URL = "https://instatweetbot.herokuapp.com/verify"
 session = dict()
-db = dict()  # you can save these values to a database
+db = dict()
 
-
+# Index WebPage - Main Page for Onboarding
 @app.route("/")
+def index():
+    image = base64.b64encode(
+        requests.get(
+            "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
+        ).content
+    )
+    image = image.decode("utf-8")
+    image = "data:image/jpeg;base64," + image
+    return render_template(
+        "index.html",
+        profile_html=f"Post Profile",
+        caption_html=f"Post Caption",
+        image=f"{image}",
+        date_html=f"DATE",
+        ecaption_html=f"Edit Caption...",
+    )
+
+
+# POST action by User
+@app.route("/", methods=["POST"])
+def generate():
+
+    # Protect against unauthorized requests
+    try:
+        url = request.form.get("url")
+        urlgiven = urlsplit(url)
+        url = urlgiven.geturl()
+        url = re.sub("https://|www.|instagram.com/p/", "", url)
+        url = url.split("/", 1)
+        shortcode = url[0]
+
+        L = instaloader.Instaloader()
+
+        try:
+            L.load_session_from_file(
+                "_professir_", "./static/session-_professir_"
+            )
+
+        except:
+            L.login(client, secret)
+            L.save_session_to_file("./static/session-_professir_")
+
+        post = Post.from_shortcode(L.context, shortcode)
+        image = base64.b64encode(requests.get(post.url).content)
+        image = image.decode("utf-8")
+        image = "data:image/jpeg;base64," + image
+
+        return render_template(
+            "index.html",
+            profile_html=f"{post.profile}",
+            caption_html=f"{post.caption}",
+            image=f"{image}",
+            date_html=f"{post.date}",
+            ecaption_html=f"{post.caption}",
+        )
+
+    except:
+        image = base64.b64encode(
+            requests.get(
+                "https://miro.medium.com/max/800/1*hFwwQAW45673VGKrMPE2qQ.png"
+            ).content
+        )
+        image = image.decode("utf-8")
+        image = "data:image/jpeg;base64," + image
+        return render_template(
+            "index.html",
+            profile_html=f"Invalid Link!!!",
+            caption_html=f"Invalid Link!!!",
+            image=f"{image}",
+            date_html=f"Invalid Link!!!",
+            ecaption_html=f"Invalid Link!!!",
+        )
+
+
+@app.route("/start")
 def send_token():
-    auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET, CALLBACK_URL)
+    auth = tweepy.OAuthHandler(twitter_client, twitter_secret, CALLBACK_URL)
 
     try:
         # get the request tokens
@@ -47,7 +126,7 @@ def get_verification():
     # get the verifier key from the request url
     verifier = request.args["oauth_verifier"]
 
-    auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
+    auth = tweepy.OAuthHandler(twitter_client, twitter_secret)
     token = session["request_token"]
     del session["request_token"]
 
@@ -67,16 +146,22 @@ def get_verification():
     db["api"] = api
     db["access_token_key"] = auth.access_token
     db["access_token_secret"] = auth.access_token_secret
-    return flask.redirect(flask.url_for("start"))
+    return flask.redirect(flask.url_for("complete"))
 
 
-@app.route("/start")
-def start():
+@app.route("/complete")
+def complete():
     # auth done, app logic can begin
     api = db["api"]
 
     # example, print your latest status posts
-    return flask.render_template("tweet.html", tweets=api.user_timeline())
+    return render_template("tweet.html", tweets=api.user_timeline())
+
+
+# Test to Ping Application
+@app.route("/test", methods=["GET"])
+def test():
+    return "Pinging Model Application!"
 
 
 if __name__ == "__main__":
